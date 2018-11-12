88ee80d8587398436770c610f4c8f4811970914c
lallhar <lfc5timesktt@gmail.com>
2018-11-12 17:50:22 +0530

    added edit and delete actions to Articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,11 +12,31 @@ class ArticlesController < ApplicationController
 		@article = Article.find(params[:id])
 	end
 
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(articles_params)
+			redirect_to @article
+		else
+			render 'edit'
+		end
+	end
+
 	def create
 		#render plain: params[:article].inspect
 		@article = Article.new(articles_params)
 		@article.save
 		redirect_to @article
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+
+		redirect_to articles_path
 	end
 
 private
